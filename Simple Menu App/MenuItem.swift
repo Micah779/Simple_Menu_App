d8012d09b106c7a44883086e4bad9e736414fa46
Micah779 <contactmaldrich@gmail.com>
2023-09-23 17:38:13 -0400

    Menu Data is now in the project, using Swift Lists and Structs

---
+++ Simple Menu App/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Simple Menu App
+//
+//  Created by Micah Aldrich on 9/23/23.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
