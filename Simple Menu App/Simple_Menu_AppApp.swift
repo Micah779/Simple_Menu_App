14936a47946a244cb3115882570c9ac626c7db19
Micah779 <contactmaldrich@gmail.com>
2023-09-23 17:03:20 -0400

    Initial Commit

---
+++ Simple Menu App/Simple_Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Menu_AppApp.swift
+//  Simple Menu App
+//
+//  Created by Micah Aldrich on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
